05db0cadafb819395e71375fd8be54abbd1c2611
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-06-01 15:32:39 +0700

    Menambahkan exception handling pada akses array

--- exceptionStandart.cpp
+++ exceptionStandart.cpp
@@ -2,3 +2,18 @@
 #include <exception>
 #include <array>
 using namespace std;
+
+
+int main()
+{
+
+    cout << "Awal program" << endl; //penanda 1
+    try
+    {
+        //deklarasi array data dengan panjang index 3
+        array <int, 3> data = {1, 2, 3};
+        //memanggil array element ke-5 yang berarti akan terjadi error
+        cout << data.at(5) << endl;
+    }
+    
+}
