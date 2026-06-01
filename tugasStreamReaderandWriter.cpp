5e33353362a38b30d116c9e7d8076c490ea2acfc
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-06-01 18:24:03 +0700

    menambahkan class TokoElektronik dengan array etalase, constructor, dan exception handling ambilProduk

--- tugasStreamReaderandWriter.cpp
+++ tugasStreamReaderandWriter.cpp
@@ -5,3 +5,34 @@
 #include <string>
 #include <limits>
 using namespace std;
+
+class TokoElektronik 
+{
+private:
+    array<string, 3> etalase;
+
+public:
+    TokoElektronik() 
+    {
+        etalase[0] = "Laptop Asus";
+        etalase[1] = "Smartphone Samsung";
+        etalase[2] = "Printer Epson";
+    }
+
+    string ambilProduk(size_t nomorRak) 
+    {
+        try 
+        {
+            return etalase.at(nomorRak);
+        }
+        catch (const out_of_range&) 
+        {
+            throw string
+            (
+                "Gagal Mengambil Barang : Rak nomor "
+                + to_string(nomorRak)
+                + " kosong atau tidak tersedia!"
+            );
+        }
+    }
+};
