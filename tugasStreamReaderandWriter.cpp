fe6c160836b75581628d847d57810423bdbea6c7
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-06-01 18:36:26 +0700

    menambahkan implementasi fungsi tambahBarang, updateBarang, dan hapusBarang pada gudang.txt

--- tugasStreamReaderandWriter.cpp
+++ tugasStreamReaderandWriter.cpp
@@ -36,3 +36,136 @@ public:
         }
     }
 };
+
+void bacaBarang() {
+    ifstream file("gudang.txt");
+
+    cout << "\n==== DAFTAR BARANG GUDANG ====\n";
+
+    string barang;
+    int no = 1;
+
+    while (getline(file, barang)) 
+    {
+        cout << no++ << ". " << barang << endl;
+    }
+
+    if (no == 1) 
+    {
+        cout << "Gudang masih kosong.\n";
+    }
+
+    file.close();
+}
+
+void tambahBarang() 
+{
+    ofstream file("gudang.txt", ios::app);
+
+    string barang;
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cout << "Masukkan nama barang: ";
+    getline(cin, barang);
+
+    file << barang << endl;
+
+    file.close();
+
+    cout << "Barang berhasil ditambahkan!\n";
+}
+
+void updateBarang() 
+{
+    ifstream file("gudang.txt");
+
+    vector<string> data;
+    string barang;
+
+    while (getline(file, barang)) 
+    {
+        data.push_back(barang);
+    }
+
+    file.close();
+
+    if (data.empty()) 
+    {
+        cout << "Tidak ada data untuk diperbarui.\n";
+        return;
+    }
+
+    bacaBarang();
+
+    int nomor;
+    cout << "Pilih nomor barang yang akan diubah: ";
+    cin >> nomor;
+
+    if (nomor < 1 || nomor > (int)data.size()) 
+    {
+        cout << "Nomor tidak valid!\n";
+        return;
+    }
+
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    cout << "Masukkan nama barang baru: ";
+    getline(cin, data[nomor - 1]);
+
+    ofstream out("gudang.txt");
+
+    for (string item : data) 
+    {
+        out << item << endl;
+    }
+
+    out.close();
+
+    cout << "Data berhasil diperbarui!\n";
+}
+
+void hapusBarang() 
+{
+    ifstream file("gudang.txt");
+
+    vector<string> data;
+    string barang;
+
+    while (getline(file, barang)) 
+    {
+        data.push_back(barang);
+    }
+
+    file.close();
+
+    if (data.empty()) 
+    {
+        cout << "Tidak ada data untuk dihapus.\n";
+        return;
+    }
+
+    bacaBarang();
+
+    int nomor;
+    cout << "Pilih nomor barang yang akan dihapus: ";
+    cin >> nomor;
+
+    if (nomor < 1 || nomor > (int)data.size()) 
+    {
+        cout << "Nomor tidak valid!\n";
+        return;
+    }
+
+    data.erase(data.begin() + (nomor - 1));
+
+    ofstream out("gudang.txt");
+
+    for (string item : data) 
+    {
+        out << item << endl;
+    }
+
+    out.close();
+
+    cout << "Data berhasil dihapus!\n";
+}
